40eaf932ceaf7c1e6d62af8eb76a3a5286295e44
Daniel Veiga <daniel.veiga@accenture.com.com>
2019-03-22 08:02:24 -0300

    Ajustes finais

--- principal.py
+++ principal.py
@@ -3,30 +3,32 @@
 from ampulheta import Ampulheta
 from miojo import Miojo
 from utils import calcula_mdc
+import sys
 
 # Captura tempo de preparo do miojo inserido pelo usuário
-tempo_preparo_miojo = input('Insira o tempo de preparo do miojo: ')
+tempo_preparo_miojo = input("Insira o tempo de preparo do miojo: ")
 
 # Captura tempo da primeira ampulheta inserido pelo usuário
-tempo_ampulheta_1 = input('Insira o tempo da primeira ampulheta '
-        '(deve ser maior que {}): '.format(tempo_preparo_miojo))
+tempo_ampulheta_1 = input("Insira o tempo da primeira ampulheta "
+        "(deve ser maior que {}): ".format(tempo_preparo_miojo))
 
 # Enquanto o usuário não digitar um valor válido, pede um novo valor
-while tempo_ampulheta_1 < tempo_preparo_miojo:
-    tempo_ampulheta_1 = input('Valor deve ser maior que o '
-        'tempo de preparo do miojo, insira o tempo da primeira '
-        'ampulheta novamente: ')
+while tempo_ampulheta_1 <= tempo_preparo_miojo:
+    tempo_ampulheta_1 = input("Valor deve ser maior que o "
+        "tempo de preparo do miojo, insira o tempo da primeira "
+        "ampulheta novamente: ")
 
 # Captura tempo da segunda ampulheta inserido pelo usuário
-tempo_ampulheta_2 = input('Insira o tempo da segunda ampulheta: ')
+tempo_ampulheta_2 = input("Insira o tempo da segunda ampulheta "
+        "(deve ser maior que {}): ".format(tempo_preparo_miojo))
 
 # Enquanto o usuário não digitar um valor válido, pede um novo valor
-while tempo_ampulheta_2 < tempo_preparo_miojo:
-    tempo_ampulheta_2 = input('Valor deve ser maior que o '
-        'tempo de preparo do miojo, insira o tempo da segunda' 
-        'ampulheta novamente: ')
+while tempo_ampulheta_2 <= tempo_preparo_miojo:
+    tempo_ampulheta_2 = input("Valor deve ser maior que o "
+        "tempo de preparo do miojo, insira o tempo da segunda" 
+        "ampulheta novamente: ")
 
-# tempo_maximo_preparo = input('Insira o maximo para preparo do miojo: ')
+# tempo_maximo_preparo = input("Insira o maximo para preparo do miojo: ")
 
 # Cria instancias para as ampulhetas e miojo
 ampulheta_1 = Ampulheta(tempo_ampulheta_1)
@@ -39,14 +41,17 @@ mdc = calcula_mdc(ampulheta_1.get_tempo(), ampulheta_2.get_tempo())
 # Verifica se é possível calcular o tempo com as ampulhetas informadas
 if miojo.get_tempo_preparo() % mdc:
     print("Não é possível obter o tempo de cozimento exato com os" 
-          "valores das ampulhetas informadas")
+          "valores das ampulhetas informadas.")
+    sys.exit()
 
 tempo_total_preparo = 0
 tempo_cozimento = 0
 tempo_ampulheta_1_aux = ampulheta_1.get_tempo() 
 tempo_ampulheta_2_aux = ampulheta_2.get_tempo()
 
-while tempo_cozimento < 60:
+# Faz iterações com as ampulhetas, utilizando o maior valor possivel para um inteiro como o maior tempo de preparo,
+# exagero apenas para testarmos o programa.
+while tempo_total_preparo < sys.maxint:
     tempo_cozimento = tempo_ampulheta_1_aux if tempo_ampulheta_1_aux < tempo_ampulheta_2_aux else tempo_ampulheta_2_aux
 
     if tempo_cozimento == miojo.get_tempo_preparo():
@@ -62,5 +67,6 @@ while tempo_cozimento < 60:
             tempo_total_preparo += tempo_ampulheta_1_aux
             tempo_ampulheta_1_aux = ampulheta_1.get_tempo()
 
-print(tempo_total_preparo)
-print(tempo_cozimento)
+print("Tempo das ampulhetas: {} e {} minutos.".format(ampulheta_1.get_tempo(), ampulheta_2.get_tempo()))
+print("Tempo total de preparo do miojo: {} minutos.".format(tempo_total_preparo))
+print("Tempo de cozimento do miojo: {} minutos.".format(tempo_cozimento))
